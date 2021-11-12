39fb8d9c2710fe737f639bd2a78a6e43e2ae57d1
DjangoReinhard <django013@nobodiesfool.com>
2021-11-12 04:58:22 +0100

    work out command writer

--- src/nml/commandwriter.h
+++ src/nml/commandwriter.h
@@ -13,6 +13,26 @@ class CommandWriter : public QObject
 public:
   explicit CommandWriter(QObject *parent = nullptr);
 
+  void abortTask();
+  void enableBlockDelete(bool enable);
+  void enableFlood(bool enable);
+  void enableMist(bool enable);
+  void enableOptionalStop(bool enable);
+  void enableSpindleOverride(double rate);
+  void jogStep(int axis, double stepSize, double speed);
+  void jogStart(int axis, double speed);
+  void jogStop(int axis);
+  void homeAxis(int jointNum);
+  void loadTaskPlan(const QString& gcodeFile);
+  void loadToolTable(const QString& toolTableFile);
+  void sendMDICommand(const QString& command);
+  void setAuto(int autoMode, int line);
+  void setFeedOverride(double rate);
+  void setRapidOverride(double rate);
+  void setSpindle(bool enable, int speed, int direction);
+  void setTaskMode(int mode);
+  void setTaskState(int state);
+
 protected:
   int sendCommand(RCS_CMD_MSG& msg);
   void sleep(double seconds);
